5a1e01f02fe15dbc9a24996495d65c2cba0b9939
mdmohiddin <mdmohiddin8000@gamil.com>
2021-09-05 10:50:27 +0600

    add Module 37-1

---
+++ explore.js
@@ -0,0 +1,10 @@
+const name = 15;
+const student = {
+      name: 'Jolil', frient: 'borsha', job: 'Business',
+      makeMovie: function (name) {
+            console.log(name);
+      }
+};
+student.makeMovie('Din raat');
+document.getElementById('name');
+
